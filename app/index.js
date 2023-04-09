f6e3e30347a95e517f5fc2ce6662f87984fd9f31
Kauan <kauanlindobonito@gmail.com>
2023-04-09 19:30:24 -0300

    cad-nome

--- app/index.js
+++ app/index.js
@@ -1,13 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 import LogIn from './pages/log-in'
+import CadNome from './pages/cad-nome';
+import { Stack } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
+  
   return (
+
+
     <View>
-      <Start/>
+
+      {/* <Start /> */}
+
+      {/* <LogIn/> */}
+
+      <CadNome />
+
     </View>
+
   )
 }
+
 
 export default Home;
