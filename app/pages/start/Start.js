aac8459cecb9d4e6419c9b99e3b81926583d57b6
Kauan <kauanlindobonito@gmail.com>
2023-05-18 15:35:49 -0300

    Tela de start padronizada

--- app/pages/start/Start.js
+++ app/pages/start/Start.js
@@ -1,4 +1,4 @@
-import {Text, View, StyleSheet,Image, TouchableOpacity} from 'react-native'
+import {Text, View, StyleSheet,Image, TouchableOpacity, SafeAreaView} from 'react-native'
 import { Stack, useRouter, Link } from 'expo-router'
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -14,32 +14,34 @@ export default function Start(){
 
 
     return (
-        <View style={styles.container}>
+        <SafeAreaView>
              <Stack.Screen 
                 options={{
                     headerShadowVisible: false,
                     headerShown: false,
                     headerTitle: ""
                 }}
-            />         
-            <View >
-                <Image style={styles.image_logo} source={logo}/>
+            />      
+
+            <View style={styles.container}>
+            <View style={{marginBottom:41}}>
+                <Image source={logo}/>
             </View>
-        
-            <TouchableOpacity onPress={LogIn}>
+
+            <TouchableOpacity style={styles.buttonTouch} onPress={LogIn}>
                 <LinearGradient style={styles.button} colors={['#00B2FF', '#1F8EFB', '#3B6FF8']}>
-                        <Text style={styles.buttonEnter}>Entrar</Text>
+                    <Text style={styles.buttonEnter}>Entrar</Text>
                 </LinearGradient>
             </TouchableOpacity>
 
-            <TouchableOpacity>
+            <TouchableOpacity style={styles.buttonTouch}>
                 <LinearGradient style={styles.button} colors={['#00B2FF', '#1F8EFB', '#3B6FF8']}>
-                        <Text style={styles.buttonCreateAccount}>Crie sua conta</Text>
+                    <Text style={styles.buttonEnter}>Crie sua conta</Text>
                 </LinearGradient>
             </TouchableOpacity>
+            </View>
             
-            
-        </View>
+        </SafeAreaView>
     
     )
 }
@@ -47,39 +49,31 @@ export default function Start(){
 const styles = StyleSheet.create({
     container:{
         backgroundColor: '#001242',
-        with: '100%',
-        height: '100%'
-        
+        width:'100%',
+        height:'100%',
+        padding:20,
+        alignItems:"center",
+        justifyContent:"center"
     },
 
-    image_logo:{
-        marginTop: '17%',
-        marginLeft: '12%',
+    buttonTouch:{
+        height: 50, 
+        width:"100%", 
+        marginBottom:20
     },
 
     button:{
-        width: '90%',
         height: 50,
-        marginLeft: '5%',
         borderRadius: 8,
-        marginTop: 30
-        
+        alignContent:"center",
+        alignItems:"center",
     },
 
     buttonEnter:{
         color: '#fff',
         fontSize: 20,
         fontWeight: 700,
-        marginLeft: '40%',
-        marginTop: 8,
+        marginTop: 9,
     },
-
-    buttonCreateAccount:{
-        color: '#fff',
-        fontSize: 20,
-        fontWeight: 700,
-        marginLeft: '30%',
-        marginTop: 8
-    }
     
 })
